83f885f802d9d5cca448edcf7c17677096507af0
Massimo Hamilton <massimo.hamilton@outlook.com>
2018-09-14 18:37:47 +0100

    Initialise SettingsService on app startup so that previously saved settings can be applied without having to visit the settings page

--- src/services/settings-service.ts
+++ src/services/settings-service.ts
@@ -3,11 +3,12 @@ import { Injectable } from "@angular/core";
 import { StyleProvider } from "../providers/style-provider";
 
 export abstract class SettingsService {
+  abstract initialise(): void;
   abstract getFontSize(): number;
   abstract setFontSize(size: number): void;
 }
 
-const DEFAULT_FONT_SIZE = 16;
+const DEFAULT_FONT_SIZE = 28;
 const FONT_SIZE_KEY = "font_size";
 
 @Injectable()
@@ -20,6 +21,9 @@ export class SettingsServiceImplementation implements SettingsService {
   ) {
     this.fontSize =
       this.storageProvider.get<number>(FONT_SIZE_KEY) || DEFAULT_FONT_SIZE;
+  }
+
+  initialise(): void {
     this.styleProvider.setFontSize(this.fontSize);
   }
 
